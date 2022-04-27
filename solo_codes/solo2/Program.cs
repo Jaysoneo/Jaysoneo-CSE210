eb5945d458afcffb0fda0a242986825981f90f39
Jaysoneo <oli21019@byui.edu>
2022-04-27 08:59:27 -0600

    I finish solo 3, and move .vscode assets

---
+++ solo_codes/solo2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace solo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your grade?");
+            string writtengrade = Console.ReadLine();
+
+            float grade = float.Parse(writtengrade);
+
+            if (grade >= 90)
+            {
+                Console.WriteLine("A");
+            }
+            else if (grade >= 80)
+            {
+                Console.WriteLine("B");
+            }
+            else if (grade >= 70)
+            {
+                Console.WriteLine("C");
+            }
+            else if (grade >= 60)
+            {
+                Console.WriteLine("D");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
+        }
+    }
+}
